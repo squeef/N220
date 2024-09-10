bbe633e57a848d0687e0dd13f4d3364c8be8b856
Nicholas Brune <n.s.brune55@gmail.com>
2024-09-10 15:32:51 -0400

    added more

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
-function simplefunction() {
+function simplefunctionOne() {
   // Code here
   document.getElementById("changeTxt").innerHTML =
     "Don't change this paragraph.";
+}
+
+simplefunctionOne();
+function simplefunctionTwo() {
+  document.querySelector("body").innerHTML +=
+    "This function does not take any parameters";
 }
